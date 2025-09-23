00cf786989a3bfb15384103fe806f9ae8d6180df
ankit <dubeysk2003@gmail.com>
2025-09-23 16:13:35 +0530

    dashhhhh1

--- src/App.js
+++ src/App.js
@@ -5,19 +5,33 @@ import ChatbotPage from "./pages/ChatbotPage";
 import Profile from "./pages/Profile";
 import MoodTrackerPage from "./pages/MoodTrackerPage";
 import TherapistNotesPage from "./pages/TherapistNotesPage";
-import CommunityPage from "./pages/CommunityPage"; // ✅ import
+import CommunityPage from "./pages/CommunityPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/chatbot" element={<ChatbotPage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/mood-tracker" element={<MoodTrackerPage />} />
-        <Route path="/therapist-notes" element={<TherapistNotesPage />} />
-        <Route path="/community" element={<CommunityPage />} /> 
+
+        {/* Protected Routes */}
+        <Route
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/chatbot" element={<ChatbotPage />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/mood-tracker" element={<MoodTrackerPage />} />
+          <Route path="/therapist-notes" element={<TherapistNotesPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+        </Route>
+
+        {/* Redirect any unknown route */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
