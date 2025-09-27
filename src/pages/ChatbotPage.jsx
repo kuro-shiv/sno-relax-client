0eff51dc055104e346b79802fc8aadee6ff095e1
kuro-shiv <dubeysk2003@gmail.com>
2025-09-27 22:17:59 +0530

    stash

--- src/pages/ChatbotPage.jsx
+++ src/pages/ChatbotPage.jsx
@@ -6,6 +6,7 @@ import "../styles/Chatbot.css";
 export default function ChatbotPage() {
   const [lang, setLang] = useState("auto"); // default = auto detect
 
+  
   return (
     <div className="chat-fullscreen">
       {/* Language selector */}
