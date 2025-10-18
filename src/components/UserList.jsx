070701ef6e63028b3c88e08a34144114710b3518
kuro-shiv <dubeysk2003@gmail.com>
2025-10-18 22:59:57 +0530

    shr

---
+++ src/components/UserList.jsx
@@ -0,0 +1,27 @@
+// write code for thisimport React from 'react';
+import PropTypes from 'prop-types';
+
+const UserList = ({ users }) => {
+    return (
+        <div>
+            <h2>User List</h2>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.name} ({user.email})
+                    </li>
+                ))} 
+            </ul>
+        </div>
+    );
+};
+
+UserList.propTypes = {
+    users: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            email: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
