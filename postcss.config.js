e17fa3355f80abf9029dc8757995dd9af9238d2b
kuro-shiv <dubeysk2003@gmail.com>
2025-11-13 17:54:09 +0530

    push

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+// PostCSS config disabled to avoid Tailwind/PostCSS plugin errors.
+// Using CRA default PostCSS pipeline.
+module.exports = {};
